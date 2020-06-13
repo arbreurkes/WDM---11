ccdadd019703a032b3daab6953228c199f1dee6e
Bart Ziengs <bzi@netcompany.com>
2020-06-13 15:06:39 +0200

    fixed project setup

--- API/Program.cs
+++ API/Program.cs
@@ -1,132 +1,20 @@
-using System.Net;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Configuration;
-using Orleans;
-using Orleans.Hosting;
-using Orleans.Configuration;
-using System.Threading.Tasks;
-using Grains;
-using Infrastructure.Interfaces;
-using Microsoft.AspNetCore.Http;
-using System;
 
-namespace ShoppingCart
+namespace API
 {
     public class Program
     {
-        public static Task Main(string[] args) =>
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.Configure((ctx, app) =>
-                    {
-                        if (ctx.HostingEnvironment.IsDevelopment())
-                        {
-                            app.UseDeveloperExceptionPage();
-                        }
-                        else
-                        {
-                            app.UseExceptionHandler(errorApp =>
-                            {
-                                errorApp.Run(async context =>
-                                {
-                                    context.Response.StatusCode = 500; // or 404 ? 
-                                    context.Response.ContentType = "application/json";
-                                    await context.Response.WriteAsync("Forbidden");
-                                    await context.Response.CompleteAsync();
-
-                                });
-                            });
-                        }
-
-                        app.UseHttpsRedirection();
-                        app.UseRouting();
-                        app.UseAuthorization();
-                        app.UseEndpoints(endpoints =>
-                        {
-                            endpoints.MapControllers();
-                        });
-                        app.UseOrleansDashboard();
-                        app.Map("/dashboard", d =>
-                        {
-                            d.UseOrleansDashboard();
-                        });
-                     
-
-                    });
-                })
-                .UseOrleans(siloBuilder =>
-                {
-                    IConfiguration conf = new ConfigurationBuilder()
-                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                    .Build();
-                    string connectionString = conf.GetConnectionString("BartsAzureTableStorage");
-                    siloBuilder
-                    //.UseLocalhostClustering()
-                    .UseAzureStorageClustering(options => options.ConnectionString = connectionString)
-                    .Configure<ClusterOptions>(opts =>
-                    {
-                        opts.ClusterId = "wdm-group11-orleans-silocluster";
-                        opts.ServiceId = "wdm-group11-orleans-api";
-                    })
-                .ConfigureApplicationParts(parts =>
-                {
-                    parts.AddApplicationPart(typeof(IOrderGrain).Assembly).WithReferences();
-                    parts.AddApplicationPart(typeof(OrderGrain).Assembly).WithReferences();
-                })
-                .Configure<GrainCollectionOptions>(options =>
-                {
-                    options.CollectionAge = TimeSpan.FromMinutes(5);
-
-                })
-                .AddAzureTableGrainStorage(
-                    name: "orderStore",
-                    configureOptions: options =>
-                    {
-                        options.UseJson = true;
-                        options.TableName = "orderStore";
-                        options.ConnectionString = connectionString;
-                    })
-                .AddAzureTableGrainStorage(
-                    name: "stockStore",
-                    configureOptions: options =>
-                    {
-                        options.UseJson = true;
-                        options.TableName = "stockStore";
-                        options.ConnectionString = connectionString;
-                    })
-                .AddAzureTableGrainStorage(
-                    name: "userStore",
-                    configureOptions: options =>
-                    {
-                        options.UseJson = true;
-                        options.TableName = "userStore";
-                        options.ConnectionString = connectionString;
-                    })
-                .UseDashboard(opts => {
-                    opts.HostSelf = false;
-                })
-                .Configure<EndpointOptions>(opts =>
-                    {
-                        opts.AdvertisedIPAddress = IPAddress.Loopback;
-                    })
-                .AddAzureTableTransactionalStateStorage(
-                    name: "transactionStore",
-                    configureOptions: options =>
-                    {
-                        options.TableName = "transactionStore";
-                        options.ConnectionString = connectionString;
-                    })
-                .UseTransactions();
-                })
-                .ConfigureServices(services =>
-                {
-                    services.AddControllers().AddNewtonsoftJson();
-                })
-            .Build()
-            .StartAsync();
+                   webBuilder.UseStartup<Startup>();
+                });
     }
 }
---
+++ API/Startup.cs
@@ -0,0 +1,79 @@
+using Grains;
+using Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using System;
+
+namespace API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddNewtonsoftJson();
+            services.AddSingleton(CreateClusterClient);
+          
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+
+            });
+        }
+
+        private IClusterClient CreateClusterClient(IServiceProvider serviceProvider)
+        {
+            IClusterClient client;
+            client = new ClientBuilder()
+                .UseLocalhostClustering()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "wdm-group11-orleans-silocluster";
+                    options.ServiceId = "wdm-group11-orleans-api";
+                })
+                .ConfigureLogging(logging => logging.AddConsole())
+                .UseAzureStorageClustering(options =>
+                    options.ConnectionString = "DefaultEndpointsProtocol=https;AccountName=wdmgroup11;AccountKey=gl81cDAOlt7o/+YoTWUc5tAg3Gn9V0j8JvHoffuR0RCyrPOHsRPSwCTmMuxYBhSrIjIbz/cvc2A28j3CUznVuQ==;EndpointSuffix=core.windows.net")
+                .ConfigureApplicationParts(p => {
+                    p.AddApplicationPart(typeof(IOrderGrain).Assembly);
+                    p.AddApplicationPart(typeof(OrderGrain).Assembly);
+                })
+                .Build();
+
+            client.Connect().Wait(); //Catch exception if it does not connects
+            Console.WriteLine("Client successfully connected to silo host \n");
+            return client;
+        }
+    }
+}
